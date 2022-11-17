cfe8c23e7d4e7f644b28b414342622d6e87bc26e
Jason Christopher <jchristopher2448@gmail.com>
2022-11-16 21:43:52 -0600

    added movie image and changing weather image

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -10,6 +10,51 @@ class Forecast extends React.Component {
     let dates = this.props.forecast.map(item => item.date);
     let descriptions = this.props.forecast.map(item => item.description);
 
+    let cloudImg = {
+      URL: 'https://images.unsplash.com/photo-1463947628408-f8581a2f4aca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
+      alt: 'cloudy image'
+    }
+    let snowImg = {
+      URL: 'https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2108&q=80',
+      alt: 'snowy image'
+    }
+    let rainImg = {
+      URL: 'https://images.unsplash.com/photo-1438449805896-28a666819a20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      alt: 'rainy image'
+    }
+    let sunImg = {
+      URL: 'https://images.unsplash.com/photo-1533324268742-60b233802eef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      alt: 'sunny image'
+    }
+    let backupImg = {
+      URL: 'https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+      alt: 'backup image'
+    }
+
+    let srcImg = description => {
+      if(description === undefined){
+        return backupImg
+      } else if(description.includes('Cloud')){
+        return cloudImg;
+      } else if(description.includes('cloud')){
+        return cloudImg;
+      } else if (description.includes('Flurries')){
+        return snowImg;
+      } else if (description.includes('snow')){
+        return snowImg;
+      } else if (description.includes('Rain')){
+        return rainImg;
+      } else if (description.includes('rain')){
+        return rainImg;
+      } else if (description.includes('Sun')){
+        return sunImg;
+      } else if (description.includes('Clear')){
+        return sunImg;
+      } else {
+        return backupImg;
+      }
+    }
+
     return (
       <>
         <Modal 
@@ -26,8 +71,8 @@ class Forecast extends React.Component {
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                  alt="First slide"
+                  src={srcImg(descriptions[0]).URL}
+                  alt={srcImg(descriptions[0]).alt}
                 />
                 <Carousel.Caption>
                   <h3>Date: {dates[0]}</h3>
@@ -37,8 +82,8 @@ class Forecast extends React.Component {
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                  alt="Second slide"
+                  src={srcImg(descriptions[1]).URL}
+                  alt={srcImg(descriptions[1]).alt}
                 />
                 <Carousel.Caption>
                   <h3>Date: {dates[1]}</h3>
@@ -48,8 +93,8 @@ class Forecast extends React.Component {
               <Carousel.Item>
                 <img
                   className="d-block w-100"
-                  src="https://images.unsplash.com/photo-1592210454359-9043f067919b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                  alt="Third slide"
+                  src={srcImg(descriptions[2]).URL}
+                  alt={srcImg(descriptions[2]).alt}
                 />
                 <Carousel.Caption>
                   <h3>Date: {dates[2]}</h3>
