8353339340ad8cd04435778fe15a882f9af6c311
Jason Christopher <jchristopher2448@gmail.com>
2022-11-18 12:59:15 -0600

    added styling to Today's Weather

--- src/components/WeatherDay.js
+++ src/components/WeatherDay.js
@@ -23,19 +23,22 @@ class WeatherDay extends React.Component {
             <Modal.Title className="dailyModalTitle">{this.props.cityData.display_name.split(',')[0]}'s Forecast | {this.props.forecast.date}</Modal.Title>
           </Modal.Header>
           <Modal.Body className="movieModalBody">
-          <img
-            className="d-block w-100"
-            src={this.props.dailyForecastImg.URL}
-            alt={this.props.dailyForecastImg.alt}
-          />
-            <section className="dailyForecastData">
-              <div className="tempFlex">
-                <p className="temp">{this.props.forecast.high}&#8457; | {this.props.forecast.low}&#8457;</p>
-                {/* <img src={this.props.dailyForecastImg.icon} alt={this.props.dailyForecastImg.alt} /> */}
-                <p className="wind"><strong>Wind:</strong> {this.props.forecast.wind}</p>
-              </div>
-              <p className="dailyDescription">{this.props.forecast.description}. High of {this.props.forecast.high}&#8457;, low of {this.props.forecast.low}&#8457;. Winds {this.props.forecast.wind}.</p>
-            </section>
+            <div className="dailyForecastPicDiv">
+              <img
+                className="d-block w-100"
+                src={this.props.dailyForecastImg.URL}
+                alt={this.props.dailyForecastImg.alt}
+                id="dailyForecastPic"
+              />
+              <article className="dailyForecastData">
+                <div className="tempAndWind">
+                  <p className="temp">{this.props.forecast.high}&#8457; | {this.props.forecast.low}&#8457;</p>
+                  {/* <img src={this.props.dailyForecastImg.icon} alt={this.props.dailyForecastImg.alt} /> */}
+                  <p className="wind"><strong>Wind:</strong> {this.props.forecast.wind}</p>
+                </div>
+                <p className="dailyDescription">{this.props.forecast.description}. High of {this.props.forecast.high}&#8457;, low of {this.props.forecast.low}&#8457;. Winds {this.props.forecast.wind}.</p>
+              </article>
+            </div>
           </Modal.Body>
           <Modal.Footer className="movieModalFooter">
             <Button className="Button" variant="secondary" onClick={this.props.handleCloseDailyForecast}>
